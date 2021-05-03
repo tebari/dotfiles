0411e4c4445f6e39fc9a13157003bd34d7050ffe
Enrique Tebari Barragan Corte <tebari@tebari.com>
2021-05-02 22:50:45 -0700

    fish - update path and ll alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,15 @@
+# Add ~/bin to path
+if test -d $HOME/bin
+    fish_add_path $HOME/bin
+end
+
+# ll alias
+if command -sq exa
+    alias ll="exa -l@ --icons"
+else
+    alias ll="ls -l"
+end
+
 # fancy prompt!
 starship init fish | source
+
