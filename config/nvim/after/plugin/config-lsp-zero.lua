8b15a947ed4e7ba20aff221f25d9cdc8d1d307a3
Enrique Tebari Barragan Corte <tebari@tebari.com>
2023-08-01 23:11:50 -0700

    neovim - enter & tab completion keybindings

--- config/nvim/after/plugin/config-lsp-zero.lua
+++ config/nvim/after/plugin/config-lsp-zero.lua
@@ -4,8 +4,21 @@ lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({buffer = bufnr})
 end)
 
+-- Configure CMP (completions)
 lsp.extend_cmp()
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+cmp.setup({
+    mapping = {
+        -- Confirm with ENTER
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        -- Tab completions
+        ['<Tab>'] = cmp_action.luasnip_supertab(),
+        ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+    }
+})
 
+-- Mason LSP Setup
 require('mason').setup({})
 require('mason-lspconfig').setup({
     -- Replace the language servers listed here
