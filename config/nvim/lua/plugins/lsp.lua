6e99a84a00ff5e8065877e71387976c3633915ad
Enrique Tebari Barragan Corte <tebari@tebari.com>
2024-07-31 16:17:04 -0700

    adjusted completion bindings and snippets

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -11,7 +11,16 @@ return {
         'hrsh7th/nvim-cmp',
         event = 'InsertEnter',
         dependencies = {
-            { 'L3MON4D3/LuaSnip' },
+            {
+                'L3MON4D3/LuaSnip',
+                dependencies = {
+                    'rafamadriz/friendly-snippets',
+                    config = function()
+                        require('luasnip.loaders.from_vscode').lazy_load()
+                    end
+                }
+            },
+            { 'saadparwaiz1/cmp_luasnip' },
             { 'hrsh7th/cmp-buffer' },
             { 'hrsh7th/cmp-path' },
 
@@ -19,17 +28,20 @@ return {
         config = function()
             local cmp = require('cmp')
             local lsp_zero = require('lsp-zero')
-            local cmp_action = lsp_zero.cmp_action()
+            local luasnip = require("luasnip")
 
             cmp.setup({
                 sources = {
                     { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
                     { name = 'buffer' },
                     { name = 'path' },
                 },
                 mapping = cmp.mapping.preset.insert({
                     -- confirm completion item
                     ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+                    ['<Tab>'] = cmp.mapping.select_next_item(),
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
                     -- trigger completion menu
                     ['<C-Space>'] = cmp.mapping.complete(),
@@ -38,9 +50,25 @@ return {
                     ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
-                    -- navigate between snippet placeholders
-                    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-                    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+                    -- Think of <c-l> as moving to the right of your snippet expansion.
+                    --  So if you have a snippet that's like:
+                    --  function $name($args)
+                    --    $body
+                    --  end
+                    --
+                    -- <c-l> will move you to the right of each of the expansion locations.
+                    -- <c-h> is similar, except moving you backwards.
+                    ['<C-l>'] = cmp.mapping(function()
+                        if luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-h>'] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { 'i', 's' }),
+
                 }),
                 formatting = lsp_zero.cmp_format({ details = true }),
                 snippet = {
