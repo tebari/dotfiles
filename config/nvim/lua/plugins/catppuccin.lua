da32c150f402aa8642b303dc0861bf2bf87166fc
Enrique Tebari Barragan Corte <tebari@tebari.com>
2024-08-01 00:01:13 -0700

    refactor lazy init and theme selection

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -1,5 +1,11 @@
 return {
-    {
-        "catppuccin/nvim", name = "catppuccin", priority = 1000
-    }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		init = function()
+            -- Load colorscheme
+            vim.cmd.colorscheme "catppuccin-mocha"
+		end,
+	},
 }
