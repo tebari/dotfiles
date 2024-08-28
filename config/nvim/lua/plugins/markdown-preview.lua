ac160720bd360ca7361e87d6005e28d1ff35df97
Enrique Tebari Barragan Corte <tebari@tebari.com>
2024-08-28 14:46:09 -0700

    add key binding for markdown preview

--- config/nvim/lua/plugins/markdown-preview.lua
+++ config/nvim/lua/plugins/markdown-preview.lua
@@ -1,8 +1,11 @@
 return {
     {
         "iamcco/markdown-preview.nvim",
-        cmd = {"MarkdownPreview", "MarkdownPreviewStop"},
+        cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
         lazy = false,
+        keys = {
+            { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview" },
+        },
         build = function()
             vim.fn["mkdp#util#install"]()
         end,
