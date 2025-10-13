e6564d642f491eaf4b26e0eb60747f6b7564706e
Enrique Tebari Barragan Corte <tebari@tebari.com>
2025-10-13 12:10:25 -0700

    add npm bin to path

--- config/fish/conf.d/00-path.fish
+++ config/fish/conf.d/00-path.fish
@@ -3,6 +3,11 @@ if test -d ~/.cargo/bin
     fish_add_path ~/.cargo/bin
 end
 
+# local node
+if test -d ~/.npm-packages/bin
+    fish_add_path ~/.npm-packages/bin
+end
+
 # add ~/.local/bin
 if test -d ~/.local/bin
     fish_add_path ~/.local/bin
