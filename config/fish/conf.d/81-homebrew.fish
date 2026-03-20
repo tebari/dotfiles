9283da06ecd28beb44fa7a06466e3dfd47792bb4
Enrique Tebari Barragan Corte <tebari@tebari.com>
2026-03-20 11:59:15 -0700

    feat(fish): support Linux homebrew installs

--- config/fish/conf.d/81-homebrew.fish
+++ config/fish/conf.d/81-homebrew.fish
@@ -1,4 +1,6 @@
-# load up homebrew in macOS
+# load up homebrew
 if command -sq /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
+else if command -sq /home/linuxbrew/.linuxbrew/bin/brew
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 end
