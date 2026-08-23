196c13fa3da504482bb594f25f6a056c6da22925
Enrique Tebari Barragan Corte <tebari@tebari.com>
2026-08-23 10:19:32 -0700

    update neovim packages

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -19,7 +19,7 @@ end
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.8",
+        tag = "v0.2.1",
         dependencies = {
             "nvim-lua/plenary.nvim",
             { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,8 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
         build = ":TSUpdate",
-        opts = {
-            ensure_installed = {
-                "bash",
-                "fish",
-                "javascript",
-                "typescript",
-                "jsdoc",
-                "lua",
-                "luadoc",
-                "rust",
-                "python",
-                "vim",
-                "diff",
-                "c",
-                "markdown",
-                "markdown_inline",
-                "query",
-                "vimdoc",
-                "groovy",
-            },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        },
-        config = function(_, opts)
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
-    -- Keeps first line of block in view when scrolling
-    {
-        "nvim-treesitter/nvim-treesitter-context",
+        lazy = false,
     },
 }
